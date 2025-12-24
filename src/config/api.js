1e2e90814e40ec7daa4be2409bd65c33eb9a1633
LeThinhPhat <lethinhphat7102003@gmail.com>
2025-12-24 09:53:17 +0700

    deloy

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://tiximax.net/api";
-// import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+  // import.meta.env.VITE_API_BASE_URL || "https://tiximax.net/api";
+  // import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+  import.meta.env.VITE_API_BASE_URL || "/api";
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: { "Content-Type": "application/json" },
