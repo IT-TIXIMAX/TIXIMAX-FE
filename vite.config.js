210cebaa9ffd17ca1bb3a6c0819a516b176b14d9
LeThinhPhat <lethinhphat7102003@gmail.com>
2026-01-06 15:45:26 +0700

    UI

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   define: {
     global: "globalThis",
     "process.env": {},
@@ -32,15 +32,6 @@ export default defineConfig({
     esbuildOptions: {
       define: {
         global: "globalThis",
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name][extname]",
       },
     },
   },
